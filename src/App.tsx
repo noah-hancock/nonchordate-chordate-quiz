5739e762e8136163841c4c555bd0bc20dcbaa073
Noah Hancock <noah@2020-5-00335.district.nlesd.ca>
2025-05-30 14:29:03 -0300

    Sort list, starting generate questions

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,85 @@ import { useState, useCallback, useEffect } from "react";
 import {
   possibleAnswersForChordates,
   possibleAnswersForNonChordates,
-  possibleChordateTypes,
-  possibleNonChordateTypes,
   chordateData,
   nonChordateData,
 } from "./data.ts";
 
+interface QuestionDataFormat {
+  [key: string]: {
+    [key: string]: string;
+  };
+}
+
+const typedChordateData: QuestionDataFormat = chordateData;
+const typedNonChordateData: QuestionDataFormat = nonChordateData;
+
 function App() {
-  const [quizLoop, setQuizLoop] = useState(false);
+  // useStates for settings
   const [practiceSet, setPracticeSet] = useState("");
+
+  // useStates for logic
+  const [allQuestions, setAllQuestions] = useState<string[][]>([]);
+
+  //useStates for display
+  const [currentQuestion, setCurrentQuestion] = useState("");
 
   // Function that handles choosing a practice set. Starts the quiz.
   const handleChoosingPracticeSet = useCallback(
     (set: string) => {
-      setQuizLoop(true);
       setPracticeSet(set);
     },
-    [setQuizLoop]
+    [setPracticeSet]
   );
 
+  // Function to generate random numbers
+  const generateRandomNumber = useCallback((max: number) => {
+    return Math.floor(Math.random() * max);
+  }, []);
+
+  // Function that shuffles the questions created by generateAllQuestions
+  const shuffleAllQuestions = useCallback(() => {
+    let questions: string[][] = [];
+    const data =
+      practiceSet === "Non-Chordate" ? typedNonChordateData : typedChordateData;
+    // Check what practice set user chose
+    for (const type in data) {
+      for (const property in data[type]) {
+        questions.push([type, property, data[type][property]]);
+      }
+    }
+
+    for (let index = questions.length - 1; index > 0; index--) {
+      const shuffleIndex: number = generateRandomNumber(index + 1);
+      [
+        ([questions[index], questions[shuffleIndex]] = [
+          questions[shuffleIndex],
+          questions[index],
+        ]),
+      ];
+    }
+    setAllQuestions(questions);
+  }, [practiceSet, generateRandomNumber, setAllQuestions]);
+
+  // This function generates and sets the current question
+  const generateCurrentQuestion = useCallback(() => {
+    const [type, property] = allQuestions[0];
+    setCurrentQuestion(`${property} of ${type} is?`);
+    setAllQuestions((previousQuestions) => previousQuestions.slice(1));
+  }, [setCurrentQuestion, allQuestions]);
+
   // This logic might become useful in the future
-  // useEffect(() => {
-  //   if (practiceSet !== "") {
-  //     console.log(practiceSet);
-  //   }
-  // }, [practiceSet]);
+  useEffect(() => {
+    // When practice set changes, start the quiz
+    if (practiceSet !== "") {
+      shuffleAllQuestions();
+      generateCurrentQuestion();
+    }
+  }, [practiceSet]);
 
   return (
     <>
-      {!quizLoop && (
+      {practiceSet === "" && (
         <>
           <h2>Non-Chordata/Chordata Quiz</h2>
           <h3>Choose a practice set.</h3>
@@ -42,7 +92,7 @@ function App() {
           </button>
         </>
       )}
-      {quizLoop && <p>True</p>}
+      {practiceSet !== "" && <p>{currentQuestion}</p>}
     </>
   );
 }
